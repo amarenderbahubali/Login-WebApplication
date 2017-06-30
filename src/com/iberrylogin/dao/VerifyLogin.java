8f78819e59520e544bfabda5744ecd30e250d658
Unitech16 <Unitech16@Unitech2-PC>
2017-06-30 15:27:09 +0530

    updated

--- src/com/iberrylogin/dao/VerifyLogin.java
+++ src/com/iberrylogin/dao/VerifyLogin.java
@@ -3,15 +3,60 @@ package com.iberrylogin.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.ResultSet;        
 import java.sql.SQLException;
 import java.sql.*;
 
 public class VerifyLogin {
     public static boolean validate(String name, String pass) {
-        //This program validates the user id and password
     	boolean status = false;
-        
+    	try{
+   Class.forName("com.mysql.jdbc.Driver");
+   Connection con=DriverManager.getConnection( "jdbc:mysql://localhost:3306/user","root","root");
+   PreparedStatement pst=con.prepareStatement("select * from login");
+   ResultSet rs=pst.executeQuery();
+   while(rs.next())
+	   {
+	   if(rs.getString(1).equals(name)&&rs.getString(2).equals(pass))
+	      {
+	status=true;   
+	return status;
+	
+   }
+	   }
+    	}
+  catch(Exception e)
+  {
+	  e.printStackTrace();
+  }
+        //This program validates the user id and password               
         return status;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/com/iberrylogin/servlets/LoginServlet.java
+++ src/com/iberrylogin/servlets/LoginServlet.java
@@ -1,11 +1,16 @@
 package com.iberrylogin.servlets;
-
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.iberrylogin.dao.VerifyLogin;
+
 
 /**
  * Servlet implementation class LoginServlet
@@ -13,7 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,8 +32,25 @@ public class LoginServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		PrintWriter out= response.getWriter();
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html");
+		String username = request.getParameter("username");
+	     String password =  request.getParameter("password");
+			if(VerifyLogin.validate(username,password))
+			{
+				RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+			rd.forward(request,response);
+			}
+			else
+			{
+				RequestDispatcher rd=request.getRequestDispatcher("error.jsp");
+			        rd.forward(request,response);
+			}
 	}
+		
+
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
